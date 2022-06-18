27ef7641e93bf81e76398a6328244410df1396e8
Giovanni <90530651+GiovanniB6@users.noreply.github.com>
2022-06-17 23:31:41 -0300

    Spawn
    Atualizado o Spawn e adicionado data de nascimento.

--- resources/[system]/spawn/web-side/jquery.js
+++ resources/[system]/spawn/web-side/jquery.js
@@ -65,11 +65,12 @@ $(document).ready(() => {
 	$(document).on("click","#createNew",function(e){
 		var name = $("#charNome").val();
 		var name2 = $("#charSobrenome").val();
+		var date = $("#charDate").val();
 		var gender = returnSeleted("gender");
 		var locate = returnSeleted("locate");
 
 		if (name != "" && name2 != ""){
-			$.post("http://spawn/newCharacter",JSON.stringify({ name: name, name2: name2, sex: gender, loc: locate }));
+			$.post("http://spawn/newCharacter",JSON.stringify({ name: name, name2: name2, sex: gender, date: date, locate: locate }));
 		}
 	});
 });
