6c9ba0b3fea513d11bb40dee5832a72c4bb6172e
Giovanni <90530651+GiovanniB6@users.noreply.github.com>
2022-07-07 02:35:06 -0300

    Banco de Dados

--- database.sql
+++ database.sql
@@ -284,6 +284,30 @@ CREATE TABLE IF NOT EXISTS `summerz_vehicles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 COMMIT;
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `wizating_stancer`
+--
+
+CREATE TABLE `wizating_stancer` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`plate` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`model` VARCHAR(55) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`owned` INT(11) NOT NULL DEFAULT '0',
+	`wheels` LONGTEXT NOT NULL COLLATE 'utf8mb4_general_ci',
+	`wheelWidth` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
+	`rideHeight` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
+	`wheelSize` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`id`) USING BTREE,
+	UNIQUE INDEX `plate` (`plate`) USING BTREE
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=24
+;
+
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
