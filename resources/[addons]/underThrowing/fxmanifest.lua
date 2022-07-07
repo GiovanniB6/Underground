d6abc262bb28709732f8eaa1277a37038f226220
Breno <ybrenog@gmail.com>
2022-07-07 17:10:18 -0300

    Addons
    Reajustado os arquivos Addons.
    - Reajustado as pastas de Itens Arremessáveis e de Armas Brancas.

---
+++ resources/[addons]/underThrowing/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version "bodacious"
+game "gta5"
+lua54 "yes"
+
+files {
+	"pedpersonality.meta",
+	"weaponanimations.meta",
+	"weaponarchetypes.meta",
+	"weapon_brick.meta",
+	"weapon_shoe.meta"
+}
+
+data_file "PED_PERSONALITY_FILE" "pedpersonality.meta"
+data_file "WEAPON_ANIMATIONS_FILE" "weaponanimations.meta"
+data_file "WEAPON_METADATA_FILE" "weaponarchetypes.meta"
+data_file "WEAPONINFO_FILE" "weapon_brick.meta"
+data_file "WEAPONINFO_FILE_PATCH" "weapon_brick.meta"
+data_file "WEAPONINFO_FILE" "weapon_shoe.meta"
+data_file "WEAPONINFO_FILE_PATCH" "weapon_shoe.meta"
